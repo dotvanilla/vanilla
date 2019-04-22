101d5b4bb1abcf8126d7ce3cfbfcaa628c64fdb8
xieguigang <xie.guigang@gcmodeller.org>
2019-04-22 18:21:58 +0800

    add dictionary api

---
+++ wasm/Symbols/DeclaredObject/JavaScriptImports/Dictionary.vb
@@ -0,0 +1,53 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+
+Namespace Symbols.JavaScriptImports
+
+    Public Module Dictionary
+
+        Public ReadOnly Property SetValue As New ImportSymbol With {
+            .ImportObject = "set",
+            .[Module] = "table",
+            .Name = "table_set",
+            .Package = NameOf(Dictionary),
+            .Result = "void",
+            .Parameters = {
+                New NamedValue(Of String)("table", "i32"),
+                New NamedValue(Of String)("key", "i32"),
+                New NamedValue(Of String)("value", "i32")
+            }
+        }
+
+        Public ReadOnly Property GetValue As New ImportSymbol With {
+            .ImportObject = "get",
+            .[Module] = "table",
+            .Name = "table_get",
+            .Package = NameOf(Dictionary),
+            .Result = "i32",
+            .Parameters = {
+                New NamedValue(Of String)("table", "i32"),
+                New NamedValue(Of String)("key", "i32")
+            }
+        }
+
+        Public ReadOnly Property RemoveValue As New ImportSymbol With {
+            .ImportObject = "delete",
+            .[Module] = "table",
+            .Name = "table_delete",
+            .Package = NameOf(Dictionary),
+            .Result = "void",
+            .Parameters = {
+                New NamedValue(Of String)("table", "i32"),
+                New NamedValue(Of String)("key", "i32")
+            }
+        }
+
+        Public ReadOnly Property Create As New ImportSymbol With {
+            .ImportObject = "create",
+            .[Module] = "table",
+            .Name = "table_new",
+            .Package = NameOf(Dictionary),
+            .Result = "i32",
+            .Parameters = {}
+        }
+    End Module
+End Namespace
