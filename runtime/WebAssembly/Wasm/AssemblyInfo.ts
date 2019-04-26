926fe80f63440c83b99132dfdcc6f28608c4b836
关倩倩 <amethyst.asuka@gcmodeller.org>
2019-04-27 00:11:37 +0800

    assemblyinfo model

---
+++ runtime/WebAssembly/Wasm/AssemblyInfo.ts
@@ -0,0 +1,25 @@
+﻿namespace vanilla {
+
+    /**
+     * The VisualBasic.NET application AssemblyInfo
+    */
+    export class AssemblyInfo {               
+
+        public constructor(
+            public AssemblyTitle: string,
+            public AssemblyDescription: string,
+            public AssemblyCompany: string,
+            public AssemblyProduct: string,
+            public AssemblyCopyright: string,
+            public AssemblyTrademark: string,
+            public Guid: string,
+            public AssemblyVersion: string,
+            public AssemblyFileVersion: string
+        ) {
+        }
+
+        public toString(): string {
+            return this.AssemblyTitle;
+        }
+    }
+}
