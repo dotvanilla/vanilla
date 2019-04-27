a879187a943da6e804635f9bf62256cfbbba79c9
xieguigang <xie.guigang@gcmodeller.org>
2019-04-27 21:03:30 +0800

    fix namespacereference errors

--- wasm/Symbols/Parser/Expressions/FuncInvokeParser.vb
+++ wasm/Symbols/Parser/Expressions/FuncInvokeParser.vb
@@ -1,48 +1,48 @@
 ﻿#Region "Microsoft.VisualBasic::cd21da1a7861379e2db62617bcdb4f97, Symbols\Parser\Expressions\FuncInvokeParser.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (I@xieguigang.me)
-    '       asuka (evia@lilithaf.me)
-    '       wasm project (developer@vanillavb.app)
-    ' 
-    ' Copyright (c) 2019 developer@vanillavb.app, VanillaBasic(https://vanillavb.app)
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (I@xieguigang.me)
+'       asuka (evia@lilithaf.me)
+'       wasm project (developer@vanillavb.app)
+' 
+' Copyright (c) 2019 developer@vanillavb.app, VanillaBasic(https://vanillavb.app)
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module FuncInvokeParser
-    ' 
-    '         Function: Argument, ArgumentSequence, fillParameters, FunctionInvoke, InvokeFunction
-    '                   ObjectInvoke, OptionalDefault
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module FuncInvokeParser
+' 
+'         Function: Argument, ArgumentSequence, fillParameters, FunctionInvoke, InvokeFunction
+'                   ObjectInvoke, OptionalDefault
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -50,6 +50,7 @@ Imports System.Runtime.CompilerServices
 Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Language
+Imports Wasm.Compiler
 
 Namespace Symbols.Parser
 
--- wasm/Symbols/Symbols.vb
+++ wasm/Symbols/Symbols.vb
@@ -1,105 +1,106 @@
 ﻿#Region "Microsoft.VisualBasic::b91916a96f38f40ae46f2486bafa2458, Symbols\Symbols.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (I@xieguigang.me)
-    '       asuka (evia@lilithaf.me)
-    '       wasm project (developer@vanillavb.app)
-    ' 
-    ' Copyright (c) 2019 developer@vanillavb.app, VanillaBasic(https://vanillavb.app)
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (I@xieguigang.me)
+'       asuka (evia@lilithaf.me)
+'       wasm project (developer@vanillavb.app)
+' 
+' Copyright (c) 2019 developer@vanillavb.app, VanillaBasic(https://vanillavb.app)
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Class CommentText
-    ' 
-    '         Properties: text
-    ' 
-    '         Function: ToSExpression, TypeInfer
-    ' 
-    '     Class LiteralExpression
-    ' 
-    '         Properties: IsLiteralNothing, Sign, type, value
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    '         Function: ToSExpression, TypeInfer
-    ' 
-    '     Class GetLocalVariable
-    ' 
-    '         Properties: var
-    ' 
-    '         Constructor: (+1 Overloads) Sub New
-    '         Function: ToSExpression, TypeInfer
-    ' 
-    '     Class SetLocalVariable
-    ' 
-    '         Properties: value, var
-    ' 
-    '         Function: ToSExpression, TypeInfer
-    ' 
-    '     Class GetGlobalVariable
-    ' 
-    '         Constructor: (+2 Overloads) Sub New
-    '         Function: ToSExpression, TypeInfer
-    ' 
-    '     Class SetGlobalVariable
-    ' 
-    '         Function: ToSExpression, TypeInfer
-    ' 
-    '     Class DeclareLocal
-    ' 
-    '         Properties: IsArray, IsObject, SetLocal
-    ' 
-    '         Function: ToSExpression
-    ' 
-    '     Class DeclareVariable
-    ' 
-    '         Properties: genericTypes, init, name, type
-    ' 
-    '         Function: TypeInfer
-    ' 
-    '     Class Parenthesized
-    ' 
-    '         Properties: Internal
-    ' 
-    '         Function: ToSExpression, TypeInfer
-    ' 
-    '     Class ReturnValue
-    ' 
-    '         Function: ToSExpression
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Class CommentText
+' 
+'         Properties: text
+' 
+'         Function: ToSExpression, TypeInfer
+' 
+'     Class LiteralExpression
+' 
+'         Properties: IsLiteralNothing, Sign, type, value
+' 
+'         Constructor: (+2 Overloads) Sub New
+'         Function: ToSExpression, TypeInfer
+' 
+'     Class GetLocalVariable
+' 
+'         Properties: var
+' 
+'         Constructor: (+1 Overloads) Sub New
+'         Function: ToSExpression, TypeInfer
+' 
+'     Class SetLocalVariable
+' 
+'         Properties: value, var
+' 
+'         Function: ToSExpression, TypeInfer
+' 
+'     Class GetGlobalVariable
+' 
+'         Constructor: (+2 Overloads) Sub New
+'         Function: ToSExpression, TypeInfer
+' 
+'     Class SetGlobalVariable
+' 
+'         Function: ToSExpression, TypeInfer
+' 
+'     Class DeclareLocal
+' 
+'         Properties: IsArray, IsObject, SetLocal
+' 
+'         Function: ToSExpression
+' 
+'     Class DeclareVariable
+' 
+'         Properties: genericTypes, init, name, type
+' 
+'         Function: TypeInfer
+' 
+'     Class Parenthesized
+' 
+'         Properties: Internal
+' 
+'         Function: ToSExpression, TypeInfer
+' 
+'     Class ReturnValue
+' 
+'         Function: ToSExpression
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Collection.Generic
+Imports Wasm.Compiler
 
 Namespace Symbols
 
