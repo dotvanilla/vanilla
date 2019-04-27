c7477d326c59e1f1d908a9bb7f1f831e7e195c1d
xieguigang <xie.guigang@gcmodeller.org>
2019-04-27 11:39:20 +0800

    The compiler type alias

--- wasm/Type/Types.vb
+++ wasm/Type/Types.vb
@@ -1,3 +1,17 @@
-﻿Public Class Types
-
-End Class
+﻿
+''' <summary>
+''' The compiler type alias
+''' </summary>
+Public Enum Types As Integer
+    any
+    i32
+    i64
+    f32
+    f64
+    [string]
+    [boolean]
+    array
+    list
+    table
+    intptr
+End Enum
