93d4f89ebb6f14fabbba2b6399bc8dc538f39b66
xieguigang <xie.guigang@gcmodeller.org>
2019-04-29 19:07:11 +0800

    add module for help find symbols

---
+++ wasm/Compiler/Link/FindSymbols.vb
@@ -0,0 +1,14 @@
+﻿Imports Wasm.Symbols
+
+Namespace Compiler
+
+    ''' <summary>
+    ''' Find symbol helper
+    ''' </summary>
+    Module FindSymbols
+
+        Public Function FindModuleMemberFunction() As FuncSignature
+
+        End Function
+    End Module
+End Namespace
