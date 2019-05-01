114efac1ec9d1efdfbe55909354e60368708de5e
xieguigang <xie.guigang@gcmodeller.org>
2019-05-01 21:02:14 +0800

    add dev note

--- test/compiler_cli.cmd
+++ test/compiler_cli.cmd
@@ -2,6 +2,9 @@
 
 SET vanilla="../Apps/vanilla"
 
+REM you can config WebAssembly output location in VisualStudio
+REM and then compile use a specific profile by setting /profile option
+REM
 %vanilla% "demo_proj\HelloWorld.vbproj" /profile "Release|AnyCPU"
 
 pause
