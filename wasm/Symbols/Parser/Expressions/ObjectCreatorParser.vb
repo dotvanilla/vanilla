1babc7b02732a477999013b1105821f41bd4d9e7
xieguigang <xie.guigang@gcmodeller.org>
2019-05-01 15:41:47 +0800

    split function

--- wasm/Symbols/Parser/Expressions/ObjectCreatorParser.vb
+++ wasm/Symbols/Parser/Expressions/ObjectCreatorParser.vb
@@ -1,53 +1,54 @@
 ﻿#Region "Microsoft.VisualBasic::7676fd9b78ccd79e1f91164825cab228, Symbols\Parser\Expressions\ObjectCreatorParser.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (I@xieguigang.me)
-    '       asuka (evia@lilithaf.me)
-    '       wasm project (developer@vanillavb.app)
-    ' 
-    ' Copyright (c) 2019 developer@vanillavb.app, VanillaBasic(https://vanillavb.app)
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (I@xieguigang.me)
+'       asuka (evia@lilithaf.me)
+'       wasm project (developer@vanillavb.app)
+' 
+' Copyright (c) 2019 developer@vanillavb.app, VanillaBasic(https://vanillavb.app)
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module ObjectCreatorParser
-    ' 
-    '         Function: AsNewObject, CreateCollection, CreateObject, GetInitializeValue
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module ObjectCreatorParser
+' 
+'         Function: AsNewObject, CreateCollection, CreateObject, GetInitializeValue
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
 Imports System.Runtime.CompilerServices
 Imports Microsoft.CodeAnalysis.VisualBasic.Syntax
 Imports Wasm.Compiler
+Imports Wasm.Symbols.JavaScriptImports
 
 Namespace Symbols.Parser
 
@@ -114,38 +115,47 @@ Namespace Symbols.Parser
 
         <Extension>
         Public Function CreateObject(create As ObjectCreationExpressionSyntax, symbols As SymbolTable) As Expression
-            Dim type = create.Type
-            Dim typeName$
+            Dim type As TypeSyntax = create.Type
 
             If TypeOf type Is GenericNameSyntax Then
-                Dim elementType As Type()
+                Return create.CreateCollectionObject(type, symbols)
+            Else
+                Throw New NotImplementedException
+            End If
+        End Function
 
-                With DirectCast(type, GenericNameSyntax).GetGenericType(symbols)
-                    typeName = .Name
-                    elementType = .Value
-                End With
+        <Extension>
+        Private Function CreateCollectionObject(create As ObjectCreationExpressionSyntax, type As TypeSyntax, symbols As SymbolTable) As Expression
+            Dim elementType As Type()
+            Dim typeName$
 
-                If typeName = "List" Then
-                    ' array和list在javascript之中都是一样的
-                    Dim listType As TypeAbstract = New TypeAbstract(elementType(Scan0)).MakeListType
-                    Dim listValues As ArraySymbol = create.Initializer.GetInitializeValue(listType, symbols)
+            With DirectCast(type, GenericNameSyntax).GetGenericType(symbols)
+                typeName = .Name
+                elementType = .Value
+            End With
 
-                    If listValues Is Nothing Then
-                        ' list没有集合元素的初始化语句
-                        ' 则只能够创建一个新的空数组
-                        Return New Array With {.type = listType, .size = Literal.i32(-1)}
-                    Else
-                        Return listValues
-                    End If
-                ElseIf typeName = "Dictionary" Then
-                    Return New ArrayTable With {
-                        .initialVal = {},
-                        .key = New TypeAbstract(elementType(Scan0)),
-                        .type = New TypeAbstract(elementType(1))
-                    }
+            If typeName = "List" Then
+                ' array和list在javascript之中都是一样的
+                Dim listType As TypeAbstract = New TypeAbstract(elementType(Scan0)).MakeListType
+                Dim listValues As ArraySymbol = create _
+                    .Initializer _
+                    .GetInitializeValue(listType, symbols)
+
+                If listValues Is Nothing Then
+                    Call symbols.doArrayImports(listType.generic(Scan0))
+
+                    ' list没有集合元素的初始化语句
+                    ' 则只能够创建一个新的空数组
+                    Return New Array With {.type = listType, .size = Literal.i32(-1)}
                 Else
-                    Throw New NotImplementedException
+                    Return listValues
                 End If
+            ElseIf typeName = "Dictionary" Then
+                Return New ArrayTable With {
+                    .initialVal = {},
+                    .key = New TypeAbstract(elementType(Scan0)),
+                    .type = New TypeAbstract(elementType(1))
+                }
             Else
                 Throw New NotImplementedException
             End If
