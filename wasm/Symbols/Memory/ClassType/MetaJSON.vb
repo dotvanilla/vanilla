78258d67f657b55df86cd7e727188ed7f3c58f6d
xieguigang <xie.guigang@gcmodeller.org>
2019-05-03 19:59:04 +0800

    meta json object

---
+++ wasm/Symbols/Memory/ClassType/MetaJSON.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+Imports Wasm.Compiler
+Imports Wasm.TypeInfo
+
+Namespace Symbols.MemoryObject
+
+    ''' <summary>
+    ''' Json data model of <see cref="ClassMeta"/>
+    ''' </summary>
+    Public Class MetaJSON : Inherits IMemoryObject
+        Implements IDeclaredObject
+
+        Public Property [Namespace] As String Implements IDeclaredObject.Module
+        Public Property ClassName As String Implements IKeyedEntity(Of String).Key
+        ''' <summary>
+        ''' A mapping table of [Field name => field type]
+        ''' </summary>
+        ''' <returns></returns>
+        Public Property Fields As Dictionary(Of String, String)
+        Public Property Methods As FuncSignature()
+
+        Public Overrides Function TypeInfer(symbolTable As SymbolTable) As TypeAbstract
+            Throw New NotImplementedException()
+        End Function
+
+        Public Overrides Function ToSExpression() As String
+            Throw New NotImplementedException()
+        End Function
+    End Class
+
+    Public Class FuncMetaJSON
+
+    End Class
+End Namespace
--- wasm/Symbols/Memory/ClassType/MetaJSON2.vb
+++
@@ -1,9 +0,0 @@
-﻿Namespace Symbols.MemoryObject
-
-    ''' <summary>
-    ''' Json data model of <see cref="ClassMeta"/>
-    ''' </summary>
-    Public Class MetaJSON
-
-    End Class
-End Namespace
