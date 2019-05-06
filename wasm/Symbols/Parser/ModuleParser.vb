752e0d6566cc68e8db2b6e11869f129a51335bd9
xieguigang <xie.guigang@gcmodeller.org>
2019-05-06 20:24:34 +0800

    parse constructor

--- wasm/Symbols/Parser/ModuleParser.vb
+++ wasm/Symbols/Parser/ModuleParser.vb
@@ -1,50 +1,50 @@
 ﻿#Region "Microsoft.VisualBasic::d281ddbbba6c0b941d8daec7ff604a8d, Symbols\Parser\ModuleParser.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (I@xieguigang.me)
-    '       asuka (evia@lilithaf.me)
-    '       wasm project (developer@vanillavb.app)
-    ' 
-    ' Copyright (c) 2019 developer@vanillavb.app, VanillaBasic(https://vanillavb.app)
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (I@xieguigang.me)
+'       asuka (evia@lilithaf.me)
+'       wasm project (developer@vanillavb.app)
+' 
+' Copyright (c) 2019 developer@vanillavb.app, VanillaBasic(https://vanillavb.app)
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module ModuleParser
-    ' 
-    '         Function: AsConstructor, (+2 Overloads) CreateModule, CreateModuleInternal, CreateUnitModule, Join
-    '                   ParseDeclares, parseEnums, ParseEnums
-    ' 
-    '         Sub: parseGlobals, parseImports
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module ModuleParser
+' 
+'         Function: AsConstructor, (+2 Overloads) CreateModule, CreateModuleInternal, CreateUnitModule, Join
+'                   ParseDeclares, parseEnums, ParseEnums
+' 
+'         Sub: parseGlobals, parseImports
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -223,7 +223,7 @@ Namespace Symbols.Parser
                 End If
             Next
 
-            Dim start As Start = main.Members _
+            Dim start As FuncSymbol = main.Members _
                 .OfType(Of ConstructorBlockSyntax) _
                 .FirstOrDefault _
                 .AsConstructor(symbolTable)
@@ -240,17 +240,22 @@ Namespace Symbols.Parser
         End Function
 
         <Extension>
-        Private Function AsConstructor(ctor As ConstructorBlockSyntax, symbols As SymbolTable) As Start
-            If ctor Is Nothing Then
-                Return New Start(symbols.currentModuleLabel)
-            Else
-                Dim moduleNew = ctor.Statements.ToArray.FunctionBody({}, symbols)
+        Private Function AsConstructor(ctor As ConstructorBlockSyntax, symbols As SymbolTable) As FuncSymbol
+            Dim moduleNew As (locals As DeclareLocal(), body As Expression()) = Nothing
 
-                Return New Start(symbols.currentModuleLabel) With {
-                    .body = moduleNew.body,
-                    .locals = moduleNew.locals
-                }
+            If Not ctor Is Nothing Then
+                moduleNew = ctor.Statements.ToArray.FunctionBody({}, symbols)
             End If
+
+            Return New FuncSymbol() With {
+                .body = moduleNew.body,
+                .locals = moduleNew.locals,
+                .isExtensionMethod = False,
+                .[module] = symbols.currentModuleLabel,
+                .name = "constructor",
+                .parameters = {},
+                .result = TypeAbstract.void
+            }
         End Function
 
         <Extension>
