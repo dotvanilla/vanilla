5f5c2f39a49f3668a8e59827c601f2fcb62730e8
xieguigang <xie.guigang@gcmodeller.org>
2019-05-07 20:28:45 +0800

    ad class and structure compres test

---
+++ test/test/differenceTest.vb
@@ -0,0 +1,38 @@
+﻿Imports test.testDifference
+
+Module ClassStructureDifferenceTest
+
+    Sub Main()
+
+        Dim c1 As New circleClass With {.r = 100, .x = 1, .y = 1}
+        ' just assign the memory pointer
+        Dim c2 = c1
+
+        Dim s1 As New circleStruct With {.y = 99, .x = .y, .r = .x * .y}
+        ' value assign of structre is value copy
+        ' this statement will create a new circlestruct object
+        Dim s2 = s1
+
+    End Sub
+
+
+    Sub New()
+        Call Main()
+    End Sub
+End Module
+
+Namespace testDifference
+
+    Public Class circleClass
+
+        Public x, y As Double
+        Public r As Integer
+
+    End Class
+
+    Public Structure circleStruct
+        Public x, y As Double
+        Public r As Integer
+    End Structure
+
+End Namespace
