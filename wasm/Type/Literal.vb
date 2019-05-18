6a6f097bb4e632af2d39be7dd38f093f34ae1820
xieguigang <xie.guigang@gcmodeller.org>
2019-05-18 14:12:16 +0800

    add dev note

--- wasm/Type/Literal.vb
+++ wasm/Type/Literal.vb
@@ -66,53 +66,38 @@ Namespace TypeInfo
         ''' <remarks>
         ''' https://github.com/WebAssembly/spec/blame/5f240d1c3e6d1f16ee8328c9cc6f3bb2e352aae9/test/core/const.wast#L128
         ''' </remarks>
-        Public Shared ReadOnly Property f64Max As New LiteralExpression With {
-            .type = TypeAbstract.f64,
-            .value = "
-                1797693134862315708145274237317043567
-                9807056752584499659891747680315726078
-                0028538760589558632766878171540458953
-                5143824642343213268894641827684675467
-                0353751698604991057655128207624549009
-                0389328944075868508455133942304583236
-                9032229481658085593321233482747978262
-                0414472316873817718091929988125040402
-                6184124858368".LineTokens _
-                              .Where(Function(s) Not s.StringEmpty) _
-                              .Select(AddressOf Strings.Trim) _
-                              .JoinBy("")
-        }
+        Public Shared ReadOnly Property f64Max As LiteralExpression
+            Get
+                Return f64(Double.MaxValue)
+            End Get
+        End Property
 
-        Public Shared ReadOnly Property f64Min As New LiteralExpression With {
-            .type = TypeAbstract.f64,
-            .value = "-
-                1797693134862315708145274237317043567
-                9807056752584499659891747680315726078
-                0028538760589558632766878171540458953
-                5143824642343213268894641827684675467
-                0353751698604991057655128207624549009
-                0389328944075868508455133942304583236
-                9032229481658085593321233482747978262
-                0414472316873817718091929988125040402
-                6184124858368".LineTokens _
-                              .Where(Function(s) Not s.StringEmpty) _
-                              .Select(AddressOf Strings.Trim) _
-                              .JoinBy("")
-        }
+        Public Shared ReadOnly Property f64Min As LiteralExpression
+            Get
+                Return f64(Double.MinValue)
+            End Get
+        End Property
 
-        Public Shared ReadOnly Property f32Max As New LiteralExpression With {
-            .type = TypeAbstract.f32,
-            .value = "340282356779733623858607532500980858880"
-        }
+        Public Shared ReadOnly Property f32Max As LiteralExpression
+            Get
+                Return f32(Single.MaxValue)
+            End Get
+        End Property
 
-        Public Shared ReadOnly Property f32Min As New LiteralExpression With {
-            .type = TypeAbstract.f32,
-            .value = "-340282356779733623858607532500980858880"
-        }
+        Public Shared ReadOnly Property f32Min As LiteralExpression
+            Get
+                Return f32(Single.MinValue)
+            End Get
+        End Property
 
         Private Sub New()
         End Sub
 
+        ''' <summary>
+        ''' Memory pointer type
+        ''' </summary>
+        ''' <param name="p"></param>
+        ''' <returns></returns>
         Public Shared Function IntPtr(p As Integer) As IntPtr
             Return New IntPtr With {
                 .expression = Literal.i32(p)
@@ -136,14 +121,22 @@ Namespace TypeInfo
         Public Shared Function f32(s As Single) As LiteralExpression
             Return New LiteralExpression With {
                 .type = New TypeAbstract("f32"),
-                .value = s.ToString.ToLower
+                .value = s.ToString("G17").ToLower
             }
         End Function
 
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="f"></param>
+        ''' <returns></returns>
+        ''' <remarks>
+        ''' https://github.com/WebAssembly/wabt/issues/1078
+        ''' </remarks>
         Public Shared Function f64(f As Double) As LiteralExpression
             Return New LiteralExpression With {
                 .type = New TypeAbstract("f64"),
-                .value = f.ToString.ToLower
+                .value = f.ToString("G17").ToLower
             }
         End Function
 
