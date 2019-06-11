2b66791bbc999ff28babe66e084d13d5feff1e1f
xieguigang <xie.guigang@gcmodeller.org>
2019-06-11 23:17:13 +0800

    fix of namespace reference errors

--- test/test/globalStarter.vb
+++ test/test/globalStarter.vb
@@ -128,7 +128,7 @@ Module treeTest
         'Next
 
 
-        '  Call projectTest("D:\repo\home\vbscripts\base64\base64.vbproj")
+        Call projectTest("D:\repo\home\vbscripts\WebGL-Demo\WebGL-Demo.vbproj")
         ' Call projectTest("D:\repo\home\vbscripts\base64\base64.vbproj")
         ' Call projectTest("D:\vanilla\test\demo_proj\HelloWorld.vbproj")
 
