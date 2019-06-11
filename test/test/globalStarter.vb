88beeeae9a60e33d46862ad361e14608e63668ac
xieguigang <xie.guigang@gcmodeller.org>
2019-06-11 23:44:36 +0800

    add object initialize test

--- test/test/globalStarter.vb
+++ test/test/globalStarter.vb
@@ -76,6 +76,7 @@ Module treeTest
 
     Sub FileTest()
         For Each file As String In {
+                "objectArrayInitlizeTest.vb",
                 "nestedTest.vb",
                 "objectArrayTest.vb",
                 "GCtest.vb",
@@ -128,7 +129,7 @@ Module treeTest
         'Next
 
 
-        Call projectTest("D:\repo\home\vbscripts\WebGL-Demo\WebGL-Demo.vbproj")
+        ' Call projectTest("D:\repo\home\vbscripts\WebGL-Demo\WebGL-Demo.vbproj")
         ' Call projectTest("D:\repo\home\vbscripts\base64\base64.vbproj")
         ' Call projectTest("D:\vanilla\test\demo_proj\HelloWorld.vbproj")
 
---
+++ test/test/test/objectArrayInitlizeTest.vb
@@ -0,0 +1,20 @@
+﻿Imports test.objectarrayInitModel
+
+Module objectArrayInitlizeTest
+
+    Dim list(100) As circle
+
+    Sub New()
+        For i As Integer = 0 To list.Length - 1
+            list(i) = New circle With {.x = VBMath.Rnd, .y = VBMath.Rnd}
+        Next
+    End Sub
+
+End Module
+
+Namespace objectarrayInitModel
+
+    Public Class circle
+        Public x, y As Single
+    End Class
+End Namespace
