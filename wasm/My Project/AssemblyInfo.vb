7476cf6447e60afa51d507470f2ac9f60fa1b4e5
xieguigang <xie.guigang@gcmodeller.org>
2019-10-03 07:28:32 +0800

    update module version

--- wasm/My Project/AssemblyInfo.vb
+++ wasm/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.7867.33")>
-<Assembly: AssemblyFileVersion("1.3.0.22")>
+<Assembly: AssemblyVersion("1.78.*")>
+<Assembly: AssemblyFileVersion("1.66.*")>
