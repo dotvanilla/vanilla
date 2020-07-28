688f9282fb9755ed8143e41a82a0ac112e5a8ddd
xieguigang <xie.guigang@gcmodeller.org>
2020-07-28 17:11:25 +0800

    Update FindSymbols.vb

--- wasm/Compiler/Link/FindSymbols.vb
+++ wasm/Compiler/Link/FindSymbols.vb
@@ -1,48 +1,48 @@
 ﻿#Region "Microsoft.VisualBasic::7660d93c62abf65715ea804572eb4528, Compiler\Link\FindSymbols.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (I@xieguigang.me)
-    '       asuka (evia@lilithaf.me)
-    '       wasm project (developer@vanillavb.app)
-    ' 
-    ' Copyright (c) 2019 developer@vanillavb.app, VanillaBasic(https://vanillavb.app)
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (I@xieguigang.me)
+'       asuka (evia@lilithaf.me)
+'       wasm project (developer@vanillavb.app)
+' 
+' Copyright (c) 2019 developer@vanillavb.app, VanillaBasic(https://vanillavb.app)
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    '     Module FindSymbols
-    ' 
-    '         Function: FindEnumValue, findMethodByFirstSignature, FindModuleGlobal, FindModuleMemberFunction, (+2 Overloads) FindTypeMethod
-    '                   handleArrayListMethods, handleStringMethods, handleTableMethods
-    ' 
-    ' 
-    ' /********************************************************************************/
+'     Module FindSymbols
+' 
+'         Function: FindEnumValue, findMethodByFirstSignature, FindModuleGlobal, FindModuleMemberFunction, (+2 Overloads) FindTypeMethod
+'                   handleArrayListMethods, handleStringMethods, handleTableMethods
+' 
+' 
+' /********************************************************************************/
 
 #End Region
 
@@ -276,23 +276,27 @@ Namespace Compiler
 
         <Extension>
         Private Function handleTableMethods(symbols As SymbolTable, name As String, typeContext As TypeAbstract) As FuncSignature
-            Dim Api As ImportSymbol = JavaScriptImports.Dictionary.Method(name)
-            Call symbols.addRequired(Api)
-            Return Api
+            Dim api As ImportSymbol = JavaScriptImports.Dictionary.Method(name)
+            Call symbols.addRequired(api)
+            Return api
         End Function
 
         <Extension>
         Private Function handleArrayListMethods(symbols As SymbolTable, name$, typeContext As TypeAbstract) As FuncSignature
-            Dim Api As ImportSymbol = JavaScriptImports.Array.Method(name, typeContext.generic(Scan0))
-            Call symbols.addRequired(Api)
-            Return Api
+            Dim api As ImportSymbol = JavaScriptImports.Array.Method(name, typeContext.generic(Scan0))
+            Call symbols.addRequired(api)
+            Return api
         End Function
 
         <Extension>
         Private Function handleStringMethods(symbols As SymbolTable, name As String) As FuncSignature
-            Dim Api = JavaScriptImports.String.Method(name)
-            Call symbols.addRequired(Api)
-            Return Api
+            If name = "ToString" Then
+                Return JavaScriptImports.String.ToString("string")
+            Else
+                Dim api = JavaScriptImports.String.Method(name)
+                Call symbols.addRequired(api)
+                Return api
+            End If
         End Function
     End Module
 End Namespace
