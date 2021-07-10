872cffb66b40fa1e16ca7f87da68c8dca708eaa5
xieguigang <xie.guigang@gcmodeller.org>
2021-07-10 15:08:09 +0800

    string constant to s-expression

--- src/WAT/CodeAnalysis/Memory/MemoryObject.vb
+++ src/WAT/CodeAnalysis/Memory/MemoryObject.vb
@@ -1,6 +1,6 @@
 ﻿Imports VanillaBasic.WebAssembly.Syntax
 
-Public Class MemoryObject
+Public MustInherit Class MemoryObject
 
     ''' <summary>
     ''' 这个对象在内存之中的起始位置
@@ -10,6 +10,9 @@ Public Class MemoryObject
     ''' 如果是一个表达式，则是一个动态资源，反之为字符串之类的静态资源
     ''' </remarks>
     Public Property MemoryPtr As MemoryPtr
+    Public Property Annotation As String
+
+    Public MustOverride ReadOnly Property sizeOf As WATSyntax
 
     Public Function [AddressOf]() As WATSyntax
         If TypeOf MemoryPtr Is StaticPtr Then
--- src/WAT/CodeAnalysis/Memory/StringLiteral.vb
+++ src/WAT/CodeAnalysis/Memory/StringLiteral.vb
@@ -1,7 +1,13 @@
-﻿
+﻿Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Text
+Imports VanillaBasic.WebAssembly.Syntax
+
 ''' <summary>
 ''' a string literal value
 ''' </summary>
+''' <remarks>
+''' just supports ASCII chars!
+''' </remarks>
 Public Class StringLiteral : Inherits MemoryObject
 
     ''' <summary>
@@ -10,4 +16,29 @@ Public Class StringLiteral : Inherits MemoryObject
     ''' <returns></returns>
     Public Property Value As String
 
+    Public Overrides ReadOnly Property sizeOf As WATSyntax
+        Get
+            Return New LiteralValue(Value.Length)
+        End Get
+    End Property
+
+    Public Function ToSExpression() As String
+        Dim lines As New List(Of String)
+        Dim nchars As Integer = Strings.Len(Value)
+
+        lines += $""
+        lines += $";; String from {MemoryPtr} with {nchars} bytes in memory"
+
+        If Not Annotation.StringEmpty Then
+            lines += ";;"
+            lines += ";; " & Annotation
+            lines += ";;"
+        End If
+
+        lines += $"(data (i32.const {MemoryPtr}) ""{Value}\00"")"
+
+        Return lines _
+            .Select(Function(line) "    " & line) _
+            .JoinBy(ASCII.LF)
+    End Function
 End Class
