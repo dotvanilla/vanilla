8aeadff8fcc6d8b2c0f2639047f4f4a857b45c64
xieguigang <xie.guigang@gcmodeller.org>
2021-07-10 15:13:33 +0800

    add expression models

---
+++ src/WAT/Syntax/DeclareLocal.vb
@@ -0,0 +1,15 @@
+﻿Namespace Syntax
+
+    Public Class DeclareLocal : Inherits WATSyntax
+
+        Public Overrides ReadOnly Property Type As CodeAnalysis.WATType
+            Get
+                Throw New NotImplementedException()
+            End Get
+        End Property
+
+        Public Overrides Function ToSExpression(env As CodeAnalysis.Environment, indent As String) As String
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
---
+++ src/WAT/Syntax/FunctionDeclare.vb
@@ -0,0 +1,21 @@
+﻿Imports VanillaBasic.WebAssembly.CodeAnalysis
+
+Namespace Syntax
+
+    Public Class FunctionDeclare : Inherits WATSyntax
+
+        Public Overrides ReadOnly Property Type As WATType
+            Get
+                Throw New NotImplementedException()
+            End Get
+        End Property
+
+        Public Property parameters As DeclareLocal()
+        Public Property locals As DeclareLocal()
+        Public Property body As WATSyntax()
+
+        Public Overrides Function ToSExpression(env As Environment, indent As String) As String
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
