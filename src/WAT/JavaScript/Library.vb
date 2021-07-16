81a43a7f4bcd479581591fe32c9b50172266e253
xieguigang <xie.guigang@gcmodeller.org>
2021-07-17 00:30:55 +0800

    how to imports api?

---
+++ src/WAT/JavaScript/Library.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Reflection
+Imports VanillaBasic.WebAssembly.Syntax
+
+Namespace JavaScript
+
+    Public Class Library
+
+        Public Shared Iterator Function [Imports](Of T As Class)() As IEnumerable(Of ImportsFunction)
+            Dim methodList As MethodInfo() = GetType(T).GetMethods
+
+            For Each api As MethodInfo In methodList
+
+            Next
+        End Function
+    End Class
+End Namespace
--- src/WAT/JavaScript/Math.vb
+++ src/WAT/JavaScript/Math.vb
@@ -1,10 +1,22 @@
-﻿Imports VanillaBasic.WebAssembly.Syntax
+﻿Namespace JavaScript
 
-Namespace JavaScript
-
+    ''' <summary>
+    ''' Math is a built-in object that has properties and methods for 
+    ''' mathematical constants and functions. It’s not a function 
+    ''' object.
+    ''' 
+    ''' https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Math
+    ''' </summary>
     Public Class Math
 
-        Public Property pow As ImportsFunction
+        ''' <summary>
+        ''' The Math.pow() function returns the base to the exponent power, as in base^exponent.
+        ''' </summary>
+        ''' <param name="base">The base number.</param>
+        ''' <param name="exponent">The exponent used to raise the base.</param>
+        ''' <returns>A number representing the given base taken to the power of the given exponent.</returns>
+        Public Declare Function pow Lib "Math" Alias "Math.Pow" (base As Double, exponent As Double) As Double
+
 
     End Class
 End Namespace
