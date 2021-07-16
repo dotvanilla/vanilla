23d55dff096d76a6ebb1f69b7ddc6b956e2bb1bc
xieguigang <xie.guigang@gcmodeller.org>
2021-07-17 01:28:55 +0800

    il code to webassembly?

---
+++ src/WAT/Compiler/ObjectManager/ObjectManager.vb
@@ -0,0 +1,22 @@
+﻿Namespace Compiler
+
+    Public Class ObjectManager
+
+        Public HeapSize As Integer
+
+        Public Function GetMemorySize() As Integer
+            Return HeapSize
+        End Function
+
+        Public Function AllocateObject(sizeof As Integer, class_id As Integer) As Integer
+            Dim offset As Integer = HeapSize
+            HeapSize = offset + sizeof
+            Return offset
+        End Function
+
+        Public Function AllocateArray(sizeof As Integer, class_id As Integer, length As Integer) As Integer
+
+        End Function
+
+    End Class
+End Namespace
