fb8c0a93963df3ea9ab500a42258b0301290f27d
xieguigang <xie.guigang@gcmodeller.org>
2021-07-17 00:06:45 +0800

    imports JavaScript math

---
+++ src/WAT/JavaScript/Math.vb
@@ -0,0 +1,10 @@
+﻿Imports VanillaBasic.WebAssembly.Syntax
+
+Namespace JavaScript
+
+    Public Class Math
+
+        Public Property pow As ImportsFunction
+
+    End Class
+End Namespace
---
+++ src/WAT/Syntax/Symbols/ImportsFunction.vb
@@ -0,0 +1,24 @@
+﻿
+Imports Microsoft.VisualBasic.Linq
+Imports VanillaBasic.WebAssembly.CodeAnalysis
+
+Namespace Syntax
+
+    Public Class ImportsFunction : Inherits WATSymbol
+
+        Public Overrides ReadOnly Property Type As WATType
+
+        Public Property Parameters As DeclareLocal()
+        Public Property ModuleName As String
+        Public Property ImportsName As String
+
+        Public Overrides Function ToSExpression(env As Environment, indent As String) As String
+            Dim arguments As String() = Parameters _
+                .SafeQuery _
+                .Select(Function(a) a.GetParameterExpression) _
+                .ToArray
+
+            Return $"(func ${Name} (import ""{ModuleName}"" ""{ImportsName}"") {arguments.JoinBy(" ")} (result {Type.UnderlyingWATType.Description}))"
+        End Function
+    End Class
+End Namespace
