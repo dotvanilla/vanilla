d357e559b8e3c067f9090fa6f76b9c1bda6a404c
xieguigang <xie.guigang@gcmodeller.org>
2021-07-17 01:04:19 +0800

    VBType

--- src/WAT/CodeAnalysis/TypeInfo/WATType.vb
+++ src/WAT/CodeAnalysis/TypeInfo/WATType.vb
@@ -1,31 +1,6 @@
 ﻿Namespace CodeAnalysis
 
-    ''' <summary>
-    ''' 在这里为了兼容用户定义的类型以及.NET Framework之中的系统类型
-    ''' </summary>
-    Public Class VBType
 
-        Public ReadOnly Property FullName As String
-        ''' <summary>
-        ''' 用户定义的类型的时候，这个属性是空值
-        ''' </summary>
-        ''' <returns></returns>
-        Public ReadOnly Property Type As Type
-
-        Sub New(fullName As String)
-            Me.FullName = fullName
-        End Sub
-
-        Sub New(type As Type)
-            Me.Type = type
-            Me.FullName = type.FullName
-        End Sub
-
-        Public Overrides Function ToString() As String
-            Return FullName
-        End Function
-
-    End Class
 
     Public Class WATType
 
