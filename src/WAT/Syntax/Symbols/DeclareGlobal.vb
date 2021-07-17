a599b273d1f9c6792c81096897e2284c42e9c551
xieguigang <xie.guigang@gcmodeller.org>
2021-07-17 16:46:06 +0800

    bugs fixed for s-expression of value

--- src/WAT/Syntax/Symbols/DeclareGlobal.vb
+++ src/WAT/Syntax/Symbols/DeclareGlobal.vb
@@ -13,7 +13,7 @@ Namespace Syntax
         Public Property Value As WATSyntax
 
         Public Overrides Function ToSExpression(env As Environment, indent As String) As String
-            Return $"(global ${Name} (mut {Type.UnderlyingWATType.ToString}) ({Value.ToSExpression(env, indent)}))"
+            Return $"(global ${Name} (mut {Type.UnderlyingWATType.ToString}) {Value.ToSExpression(env, indent)})"
         End Function
     End Class
 End Namespace
